30bd5768b1746972ba01e201baa16e138238a947
Darren Armstrong <darren.armstrong85@gmail.com>
2015-07-03 00:01:15 +0100

    Return a sensible K value like we promised in C api

--- lib/ts.c
+++ lib/ts.c
@@ -2,17 +2,17 @@
 #include <sys/timerfd.h>
 
 K start(K dummy) {
-	
+	return (K) 0;
 }
 
 K stop(K dummy) {
-	
+	return (K) 0;
 }
 
 K setnext (K dummy) {
-	
+	return (K) 0;
 }
 
 K getnext (K dummy) {
-	
+	return (K) 0;
 }
