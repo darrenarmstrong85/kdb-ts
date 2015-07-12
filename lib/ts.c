ee2a892b4ee72f8215f610d0bf5b7da55e34d71d
Darren Armstrong <darren.armstrong85@gmail.com>
2015-07-12 01:05:03 +0100

    Use c++14, call timerfd_create in start, close in stop

--- lib/ts.c
+++ lib/ts.c
@@ -1,12 +1,22 @@
 #include"k.h"
 #include <sys/timerfd.h>
+#include <unistd.h>
+
+int timerfd=0;
 
 extern "C" {
 	K start(K dummy) {
+		if(not timerfd) {
+			timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+		}
+
 		return (K) 0;
 	}
 
 	K stop(K dummy) {
+		if(not timerfd) {
+			close(timerfd);
+		}
 		return (K) 0;
 	}
 
