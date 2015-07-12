69433cc77c18cb1f79a55b9abf90c2934ff8d913
Darren Armstrong <darren.armstrong85@gmail.com>
2015-07-12 01:32:16 +0100

    Add code to get next event time, plus debugging output

--- lib/ts.c
+++ lib/ts.c
@@ -1,6 +1,7 @@
 #include"k.h"
 #include <sys/timerfd.h>
 #include <unistd.h>
+#include <iostream>
 
 int timerfd=0;
 
@@ -25,6 +26,14 @@ extern "C" {
 	}
 
 	K getnext (K dummy) {
-		return (K) 0;
+		if(not timerfd) return ktj(-KN,nj);
+
+		struct itimerspec spec;
+		timerfd_gettime(timerfd, &spec);
+		std::cout << spec.it_interval.tv_sec << "\t" << spec.it_interval.tv_nsec << "\t"
+				  << spec.it_value.tv_sec << "\t" << spec.it_value.tv_nsec << std::endl;
+		J ns = (1e9 * spec.it_value.tv_sec) + spec.it_value.tv_nsec;
+		K timeuntil = ktj(-KN, ns!=0 ? ns : nj);
+		return timeuntil;
 	}
 }
