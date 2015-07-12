235976cb181f80a3ef7f93795a14da62d0cdfde8
Darren Armstrong <darren.armstrong85@gmail.com>
2015-07-12 15:28:33 +0100

    Signal errors in start/stop/get, first working versions of get/set.
      - Signal errors in start/stop/set when timer is in incorrect state.
    
      - Use up/pu functions to convert between kdb and unix timestamp formats in get/set.
    
      - Fix logic in stop, only close if timerfd!=0.
    
      - Don't print debugging output now that we have fixed things
    
      - Time returned in get is a timestamp, not a timespan
    
      - Move setnext below getnext so that we use getnext as return value.
    
      - Define it_interval values in setnext

--- lib/ts.c
+++ lib/ts.c
@@ -5,26 +5,27 @@
 
 int timerfd=0;
 
+// from code.kx.com
+I up(J f){return (f/8.64e13+10957)*8.64e4;}  // unix from kdb+ timestamp
+J pu(I u){return 8.64e13*(u/8.64e4-10957);}  // kdb+ timestamp from unix
+
+
 extern "C" {
 	K start(K dummy) {
-		if(not timerfd) {
-			timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+		if(timerfd) {
+			return krr((S) "Timer already started");
 		}
 
+		timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
 		return ki(timerfd);
 	}
 
 	K stop(K dummy) {
 		if(not timerfd) {
-			close(timerfd);
+			return krr((S) "Timer not started");
 		}
+		close(timerfd);
 		return ki(timerfd);
-	}
-
-	K setnext (K dummy) {
-		if(not timerfd) return krr((S)"Timer not started");
-
-		return (K) 0;
 	}
 
 	K getnext (K dummy) {
@@ -32,10 +33,24 @@ extern "C" {
 
 		struct itimerspec spec;
 		timerfd_gettime(timerfd, &spec);
-		std::cout << spec.it_interval.tv_sec << "\t" << spec.it_interval.tv_nsec << "\t"
-				  << spec.it_value.tv_sec << "\t" << spec.it_value.tv_nsec << std::endl;
-		J ns = (1e9 * spec.it_value.tv_sec) + spec.it_value.tv_nsec;
-		K timeuntil = ktj(-KN, ns!=0 ? ns : nj);
+		J ns = pu(spec.it_value.tv_sec) + spec.it_value.tv_nsec;
+		K timeuntil = ktj(-KP, ns!=0 ? ns : nj);
 		return timeuntil;
 	}
+
+	K setnext (K nexttime) {
+		if(not timerfd) return krr((S)"Timer not started");
+
+		itimerspec newtimer;
+
+		newtimer.it_value.tv_sec = up(nexttime->j);
+		newtimer.it_value.tv_nsec = nexttime->j%(J)1e9;
+		newtimer.it_interval.tv_sec = 0;
+		newtimer.it_interval.tv_nsec = 0;
+
+		timerfd_settime(timerfd, 0, &newtimer, nullptr);
+
+		return getnext((K) 0);
+	}
+
 }
